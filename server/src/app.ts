e76a49309db6f1417fbafb0e560f6dc97b2dd483
Neenus Gabriel <hello@neenus.com>
2026-06-04 15:15:30 +0000

    feat: register people and companies routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,6 +4,8 @@ import dotenv from 'dotenv';
 import { authRouter } from './routes/auth.routes.js';
 import { usersRouter } from './routes/users.routes.js';
 import { documentsRouter } from './routes/documents.routes.js';
+import { peopleRouter } from './routes/people.routes.js';
+import { companiesRouter } from './routes/companies.routes.js';
 
 dotenv.config({ path: './config/.env' });
 
@@ -14,3 +16,5 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', usersRouter);
 app.use('/api/v1/documents', documentsRouter);
+app.use('/api/v1/people', peopleRouter);
+app.use('/api/v1/companies', companiesRouter);
