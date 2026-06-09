d50f659a4b642b6cd58a33ef73258b9e1eab81b5
Neenus Gabriel <hello@neenus.com>
2026-06-09 07:09:06 +0000

    fix(jset.setup.cjs): use AUTH_API_KEY env var to match env var in .env

--- server/jest.setup.cjs
+++ server/jest.setup.cjs
@@ -1,3 +1,3 @@
 process.env.AUTH_SERVICE_URL = process.env.AUTH_SERVICE_URL || 'http://localhost:5001';
 process.env.APP_NAME = process.env.APP_NAME || 'minutes-app';
-process.env.NR_AUTH_API_KEY = process.env.NR_AUTH_API_KEY || 'test-api-key';
+process.env.AUTH_API_KEY = process.env.AUTH_API_KEY || 'test-api-key';
